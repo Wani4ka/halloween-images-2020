4ca6ba8d3b3bed05b5b19a77e9af54688909ded8
Wani4ka <contact.wani4ka@gmail.com>
2020-10-09 22:18:24 +0300

    Changed base url format

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const config = {}
 module.exports = config
 
 config.appPort = process.env.PORT || 5000
-config.appBaseUrl = process.env.BASE_URL + ':' + config.appPort + '/'
+config.appBaseUrl = process.env.BASE_URL
 
 config.steamApiKey = process.env.STEAM_APIKEY
